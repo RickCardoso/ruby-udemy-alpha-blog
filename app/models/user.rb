2955da14aba7496e2dff9c5195ef78401e76f495
Ricardo Cardoso <ricardo.cardoso@nexar.app>
2022-11-04 08:12:03 -0300

    feat: add user id to articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
-class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
-  
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+class User < ApplicationRecord
+  has_many :articles
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 },format: { with: VALID_EMAIL_REGEX }
 end
