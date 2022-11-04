171a33fdca40559e01d1e943ae009b3918e8b07f
Ricardo Cardoso <ricardo.cardoso@nexar.app>
2022-11-04 10:23:18 -0300

    feat: add user sign up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome, #{@user.username}! You have successfully signed up."
+      redirect_to articles_path
+    else  
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
